498f0c53ecbc28ac9d5718126f2f948fbbb6ec44
Peter Ton <pnton@ucsd.edu>
2019-03-31 20:53:33 -0700

    Removed extraneous files

--- load-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-REQUESTNUMBER=500
-MAXCONCURRENTREQUESTS=50
-
-shopt -s globstar
-for file in hotdogs/test/1/*.jpg hotdogs/test/2/*.jpg hotdogs/train/1/*.jpg hotdogs/train/2/*.jpg hotdogs/valid/1/*.jpg hotdogs/valid/2/*.jpg ; do
-    nohup ab -n $REQUESTNUMBER -c $MAXCONCURRENTREQUESTS -p $file -T application/x-www-form-urlencoded http://nothotdog.elumitas.com:5000/is-hot-dog &
-done
